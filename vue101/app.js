27e55a93c3ed9ec54832969f5ef978685c89b16e
osm3rr <osm3rr@adakademy.com>
2025-12-06 16:21:56 -0400

    primera vue app

---
+++ vue101/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            goalsA: 'Ir a la playa',
+            goalsB: 'Ir a la montaña',
+        }
+    }
+});
+
+app.mount('#goals');
+
+

e57ea6e2a9d0c7e352e98e76e33418e874e9834a
osm3rr <osm3rr@adakademy.com>
2025-12-06 17:32:11 -0400

    methods

--- vue101/app.js
+++ vue101/app.js
@@ -3,6 +3,18 @@ const app = Vue.createApp({
         return {
             goalsA: 'Ir a la playa',
             goalsB: 'Ir a la montaña',
+            vueLink: 'https://vuejs.org/',
+        }
+    },
+    methods: {
+        changeGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                
+                return this.goalsA;
+            } else {
+                return this.goalsB;
+            }
         }
     }
 });
